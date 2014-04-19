f4a22ac140cd2098e0ffcb9dab1f8e91e36b00dc
BAStyles <aleksandar.balaneskovic93@gmail.com>
2014-04-19 02:34:28 +0200

    Evas-Responsive Ghost Theme

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+    });
+    var img = document.querySelector('.post img:first-of-type');
+
+if(img) {
+    var src = img.getAttribute('src');
+    var div = document.querySelector('div');
+
+	  
+	}
+
+}(jQuery));
